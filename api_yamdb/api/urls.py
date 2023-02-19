2ae704257cfee27be79a5776b57ae8dcec6ef961
Irina-Saf <sh-irishka2008@mail.ru>
2023-02-19 11:36:22 +0300

    изменения URL

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -5,18 +5,18 @@ from .views import (UserViewSet, GetToken, Signup, CategoryViewSet,
                     TitleViewSet, GenreViewSet, ReviewViewSet, CommentViewSet)
 
 router = DefaultRouter()
-router.register(r'users/', UserViewSet, basename='users')
-router.register(r'categories/', CategoryViewSet, basename='category')
-router.register(r'genres/', GenreViewSet, basename='genre')
-router.register(r'titles/', TitleViewSet, basename='title')
+router.register(r'users', UserViewSet, basename='users')
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'genres', GenreViewSet, basename='genre')
+router.register(r'titles', TitleViewSet, basename='title')
 router.register(r'titles/(?P<title_id>/reviews', ReviewViewSet)
 router.register(r'titles/(?P<title_id>/reviews/(?P<review_id>\d+)/comments',
                 CommentViewSet, basename='comments')
 
 
 api_users_pattern = [
-    path('/auth/token/', GetToken.as_view(), name='get_token'),
-    path('/auth/signup/', Signup.as_view(), name='signup'),
+    path('auth/token/', GetToken.as_view(), name='get_token'),
+    path('auth/signup/', Signup.as_view(), name='signup'),
 ]
 urlpatterns = [
     path('', include(router.urls)),
