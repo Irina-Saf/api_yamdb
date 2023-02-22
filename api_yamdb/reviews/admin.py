6432e2aa45db99a855c2222a0cce1fe2091ae4ac
Irina-Saf <sh-irishka2008@mail.ru>
2023-02-22 16:35:44 +0300

    befor 11 tests

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,54 +1,54 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-from .models import Category, Title, Genre, User
-
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = (
-        'pk',
-        'name',
-        'slug'
-    )
-    search_fields = ('name',)
-    empty_value_display = '-пусто-'
+# from .models import Category, Title, Genre, User
 
 
-class TitleAdmin(admin.ModelAdmin):
-    list_display = (
-        'pk',
-        'name',
-        'year',
-        'rating',
-        'description',
-        'category',
-
-    )
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'pk',
+#         'name',
+#         'slug'
+#     )
+#     search_fields = ('name',)
+#     empty_value_display = '-пусто-'
 
 
-class GenreAdmin(admin.ModelAdmin):
-    list_display = (
-        "pk",
-        "name",
-        "slug",
+# class TitleAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'pk',
+#         'name',
+#         'year',
+#         'rating',
+#         'description',
+#         'category',
 
-    )
-
-    empty_value_display = "-пусто-"
-
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        'pk',
-        'username',
-        'first_name',
-        'last_name',
-        'email',
-        'bio'
-
-    )
+#     )
 
 
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Title, TitleAdmin)
-admin.site.register(Genre, GenreAdmin)
-admin.site.register(User, UserAdmin)
+# class GenreAdmin(admin.ModelAdmin):
+#     list_display = (
+#         "pk",
+#         "name",
+#         "slug",
+
+#     )
+
+#     empty_value_display = "-пусто-"
+
+
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'pk',
+#         'username',
+#         'first_name',
+#         'last_name',
+#         'email',
+#         'bio'
+
+#     )
+
+
+# admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Title, TitleAdmin)
+# admin.site.register(Genre, GenreAdmin)
+# admin.site.register(User, UserAdmin)
