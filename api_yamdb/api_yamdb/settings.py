2ccd270447efedcdf3e0f667bcdee8a7fb67e614
Irina-Saf <sh-irishka2008@mail.ru>
2023-02-25 13:02:56 +0300

    очищен лишний комментарий

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -105,7 +105,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
-# УДАЛИТЬ НИЖНЮЮ СТРОКУ!!!!!
 CSV_FILES_DIR = os.path.join(BASE_DIR, 'static/data/')
 
 AUTH_USER_MODEL = 'reviews.User'
